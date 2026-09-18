2180a57b4a4cde79e4af86cf3b612216b5bb3753
Manish Sinde <25io1na83@mitsgwl.ac.in>
2026-09-18 23:01:31 +0530

    Fix production API configuration

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,23 +1,20 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 });
 
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('jwtToken');
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwtToken');
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default apiClient;
